a7119381b51618bf92668fbe6bf0d20280fb7d5b
root <paulorubebe@yahoo.com>
2023-11-10 06:17:04 +0100

    add set up file in common directory for integration tests

---
+++ target/debug/deps/integration_test-f54350f2d22a4b64.d
@@ -0,0 +1,5 @@
+/root/my_rust_journey/target/debug/deps/integration_test-f54350f2d22a4b64: tests/integration_test.rs
+
+/root/my_rust_journey/target/debug/deps/integration_test-f54350f2d22a4b64.d: tests/integration_test.rs
+
+tests/integration_test.rs:
---
+++ tests/integration_test.rs
@@ -0,0 +1,6 @@
+use my_rust_journey;
+
+#[test]
+fn it_adds_two_numbers() {
+    assert_eq!(4, my_rust_journey::add(2, 2));
+}
