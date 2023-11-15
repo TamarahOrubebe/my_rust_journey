17f6a519ef729f46cfeee03c9731acff2377e747
root <paulorubebe@yahoo.com>
2023-11-15 09:53:45 +0100

    adding work spaces

--- target/debug/deps/integration_test-f54350f2d22a4b64.d
+++ target/debug/deps/integration_test-f54350f2d22a4b64.d
@@ -1,5 +1,6 @@
-/root/my_rust_journey/target/debug/deps/integration_test-f54350f2d22a4b64: tests/integration_test.rs
+/root/my_rust_journey/target/debug/deps/integration_test-f54350f2d22a4b64: tests/integration_test.rs tests/common/mod.rs
 
-/root/my_rust_journey/target/debug/deps/integration_test-f54350f2d22a4b64.d: tests/integration_test.rs
+/root/my_rust_journey/target/debug/deps/integration_test-f54350f2d22a4b64.d: tests/integration_test.rs tests/common/mod.rs
 
 tests/integration_test.rs:
+tests/common/mod.rs:
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,9 @@
 use my_rust_journey;
 
+mod common;
+
 #[test]
 fn it_adds_two_numbers() {
+    common::setup();
     assert_eq!(4, my_rust_journey::add(2, 2));
 }
