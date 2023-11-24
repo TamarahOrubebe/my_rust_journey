cb5d7f83386e7f7b9426ad49fa257e083c1c0b48
root <paulorubebe@yahoo.com>
2023-11-24 10:12:58 +0100

    Create a simple single threaded server to listen to requests and send responses

---
+++ hello_server/target/debug/deps/hello_server-9a4b6c8892f57961.d
@@ -0,0 +1,5 @@
+/root/my_rust_journey/hello_server/target/debug/deps/hello_server-9a4b6c8892f57961: src/main.rs
+
+/root/my_rust_journey/hello_server/target/debug/deps/hello_server-9a4b6c8892f57961.d: src/main.rs
+
+src/main.rs:
---
+++ hello_server/target/debug/hello_server.d
@@ -0,0 +1,1 @@
+/root/my_rust_journey/hello_server/target/debug/hello_server: /root/my_rust_journey/hello_server/src/main.rs
