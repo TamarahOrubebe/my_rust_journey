9f188be91477f94930c180cccc1f3cfd52e6b44a
root <paulorubebe@yahoo.com>
2023-11-29 16:06:33 +0100

    Add better error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,43 +3,38 @@ use rand::Rng;
 use std::cmp::Ordering;
 use colored::*;
 
+fn main () {
+    println!("Welcome to the guessing game where you guess the secret number");
 
-fn main() {
-    println!("The guessing game!");
-
-    
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The sceret number is: {secret_number}");
-
-    
     loop {
-            println!("Please enter a number!");
+        println!("Please input your number");
 
-           let mut guess = String::new();
+        let mut guess = String::new();
 
-            io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-            let guess: u32 = match guess.trim().parse() {
-                Ok(num) => num,
-                Err(_) => continue,
-            };
+        println!("The secret number is {}", &secret_number);
 
-            println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess).expect("Error reading input");
 
-            match guess.cmp(&secret_number) {
-                Ordering::Less => println!("{}", "Too low!".red()),
-                Ordering::Greater => println!("{}", "Too big!".red()),
-                Ordering::Equal => {
-                    println!("{}", "You win!".green());
-                    break;
-                }
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too low!".red()),
+            Ordering::Greater => println!("{}", "Too high!".red()),
+            Ordering::Equal =>  {
+                println!("{}", "You win!".green());
+                break;
             }
-
+            
+        }
     }
    
-    
-}
 
+}
